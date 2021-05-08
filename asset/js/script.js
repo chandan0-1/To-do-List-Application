a85f1b6eef804b58a1f275e7245a40a600371f82
chandan0-1 <chandanofficial.cn@gmail.com>
2021-05-08 19:40:48 +0530

    Completed till Insertion of Task

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,2 +1,47 @@
 let searchBox = document.getElementById("search-box");
-let submit = document.getElementById("submit");
+let submit = document.getElementById("Submit");
+showEvent();
+
+submit.addEventListener("click", function(){
+  Data = searchBox.value; 
+  
+  console.log(Data.length);
+  if (Data.trim().length > 0){
+    let task = localStorage.getItem("Tasks");
+    if (task == null){
+      taskObj = [];
+    }
+    else{
+      taskObj = JSON.parse(task)//converting to the Obj so that 
+      // can apply various method because in the local storage
+      // everthing stored as string
+    }
+    taskObj.push(Data);
+  }
+  localStorage.setItem("Tasks", JSON.stringify(taskObj));//since everting converted to the string in the local storage
+  showEvent();
+})
+
+
+function showEvent(){
+    let task = localStorage.getItem("Tasks");
+    if (task == null){
+      taskObj = [];
+    }
+    else{
+      taskObj = JSON.parse(task);
+    }
+    let content = "";
+    let tableDataList = document.getElementById("tableList");
+
+    taskObj.forEach((item, i) => {
+      content += `<tr>
+      <th>${i+1}</th>
+      <td>${ item }</td>
+      <td>Edit</td>
+      <td>Delete</td>
+    </tr>`;
+    });
+    tableDataList.innerHTML = content;
+}
+
